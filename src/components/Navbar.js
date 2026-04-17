502c7642b2be1172fc48b8fd5020db14e6ff8e71
unknown <yabget19@gmail.com>
2026-04-18 00:22:03 +0300

    Add Works section, roadmap components, and update Home & Navbar styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,6 +3,12 @@ import { useState } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../assets/images/logo.png'
 import '../styles/navbar.css'
+import { MdMenuBook } from "react-icons/md";
+import { BsChatDots } from "react-icons/bs";
+import { IoNotificationsOutline } from "react-icons/io5";
+import { RxDashboard } from "react-icons/rx";
+import { MdPayment } from "react-icons/md";
+import { AiOutlineQuestionCircle } from "react-icons/ai";
 import { FaUserCircle, FaBars, FaTimes } from "react-icons/fa";
 export default function Navbar() {
   const [menuOpen,setMenuOpen]=useState(false)
@@ -14,13 +20,16 @@ export default function Navbar() {
         </div>
         <div className={menuOpen?'hidden-menubar':'links'}>
             <Link to="/">Home</Link>
-            <Link to='/courses'>Courses</Link>
-            <Link to='/dashboard'>Dashboard</Link>
-            <Link to='/chat'>Chat</Link>
-            <Link to='/subscription'>Subscription</Link>
-            <Link to='/help'>Help</Link>
+            <Link to='/courses'><MdMenuBook size={20} />Courses</Link>
+            <Link to='/dashboard'> <RxDashboard size={20} />Dashboard</Link>
+            <Link to='/chat'> <BsChatDots size={20} />Chat</Link>
+            <Link to='/subscription'><MdPayment size={20} />Subscription</Link>
+            <Link to='/help'><AiOutlineQuestionCircle size={20} />Help</Link>
         </div>
         <div className='register'>
+          <div className='notification'>
+            <IoNotificationsOutline size={22}/>
+          </div>
           {register?<div className='profile'>
                          <FaUserCircle size={30} />
                     </div>:
